5d54bda2f6e0cdfedce6f28d9cf1e83fb1603ec8
Tu Nombre <tu-email@ejemplo.com>
2025-08-28 09:24:26 +0200

    Update version to 1.0.2 in README, main plugin file, and class definition. Corrected WP references and updated website link to padresenlanube.com.

--- includes/class-userspn.php
+++ includes/class-userspn.php
@@ -52,7 +52,7 @@ class USERSPN {
 		if (defined('USERSPN_VERSION')) {
 			$this->version = USERSPN_VERSION;
 		} else {
-			$this->version = '1.0.1';
+			$this->version = '1.0.2';
 		}
 
 		$this->plugin_name = 'userspn';
