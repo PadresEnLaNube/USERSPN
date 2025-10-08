18dd46c4d2e7a0e57208a9338fe13a73db65aec2
PadresEnLaNube <info@padresenlanube.com>
2025-10-08 20:12:51 +0200

    Update version to 1.0.6 in README, main plugin file, and class definitions. Adjusted WP references to reflect the new website link. Ensured consistency across versioning in all relevant files.

--- includes/class-userspn.php
+++ includes/class-userspn.php
@@ -52,7 +52,7 @@ class USERSPN {
 		if (defined('USERSPN_VERSION')) {
 			$this->version = USERSPN_VERSION;
 		} else {
-			$this->version = '1.0.5';
+			$this->version = '1.0.6';
 		}
 
 		$this->plugin_name = 'userspn';
