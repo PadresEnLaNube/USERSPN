b922f33c7d35d020b589003b014903d6c10c75eb
PadresEnLaNube <info@padresenlanube.com>
2026-01-24 18:15:30 +0100

    fix: Update lost password URL generation to use the correct function and enhance CSS for textarea z-index to ensure proper layering in the user interface.

--- includes/class-userspn-settings.php
+++ includes/class-userspn-settings.php
@@ -987,7 +987,7 @@ class USERSPN_Settings
   public function userspn_lostpassword_url($lostpassword_url, $redirect)
   {
     $nonce = wp_create_nonce('userspn-nonce');
-    return esc_url(add_query_arg('userspn_ajax_nopriv_nonce', $nonce, home_url('wp-login.php?action=lostpassword')));
+    return esc_url(add_query_arg('userspn_ajax_nopriv_nonce', $nonce, lostpassword_url(home_url())));
   }
 
   public function userspn_add_nonce_to_lostpassword_form()
